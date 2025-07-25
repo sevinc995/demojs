e871b0832c170276b08fc0d9cb942f71a11871d9
sevinc995 <sevincseferova25@gmail.com>
2025-07-25 10:11:57 +0000

    domeproject

--- demojs/app.js
+++ demojs/app.js
@@ -1,4 +1,13 @@
 "use strict";
 $(document).ready(function () {
+    const myImage = document.querySelector("img");
 
+    myImage.addEventListener("click", () => {
+        const mySrc = myImage.getAttribute("src");
+        if (mySrc === "images/firefox-icon.jpeg") {
+            myImage.setAttribute("src", images/firefox2.jpeg);
+        } else {
+            myImage.setAttribute ("src", "images/firefox-icon.jpeg");
+        }
+    });
 });
