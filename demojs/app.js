76867c65166e56ad01978c38822b2e3d97973e1b
sevinc995 <sevincseferova25@gmail.com>
2025-08-01 12:57:33 +0000

    adding

--- demojs/app.js
+++ demojs/app.js
@@ -1,14 +1,29 @@
 "use strict";
 $(document).ready(function () {
-    const myImage = document.querySelector("img");
+        const myImage = document.querySelector("img");
 
-    myImage.addEventListener("click", () => {
-        const mySrc = myImage.getAttribute("src");
-        if (mySrc === "images/firefox-icon.jpeg") {
-        myImage.setAttribute("src", "images/firefox2.jpeg");
+        myImage.addEventListener("click", () => {
+            const mySrc = myImage.getAttribute("src");
+            if (mySrc === "images/firefox-icon.jpeg") {
+            myImage.setAttribute("src", "images/firefox2.jpeg");
+            } else {
+            myImage.setAttribute("src", "images/firefox-icon.jpeg");
+            }
+        });
+
+
+
+    const listItems = document.querySelectorAll("li");
+
+    function toggleDone(e) {
+        if (!e.target.className) {
+            e.target.className = "done";
         } else {
-        myImage.setAttribute("src", "images/firefox-icon.jpeg");
+            e.target.className = "";
         }
-    });
+    }
 
+    listItems.forEach((item) => {
+        item.addEventListener("click", toggleDone);
+    });
 });
