3866c4f0e2bcdfcf4446d7cce19dde34f7857fe6
sevinc995 <sevincseferova25@gmail.com>
2025-08-29 10:46:38 +0000

    change

--- demojs/app.js
+++ demojs/app.js
@@ -27,10 +27,20 @@ $(document).ready(function () {
         item.addEventListener("click", toggleDone);
     });
 
-    // Store a reference to the <h2> in a variable
-    const myHeading = document.querySelector("h2");
-    // Update the text content of the <h2>
-    myHeading.textContent = "Hello world!";
 
-   
+    // let myButton = document.querySelector("button");
+    // let myHeading = document.querySelector("h1");
+
+    // function setUserName() {
+    //     const myName = prompt("Please enter your name.");
+    //     localStorage.setItem("name", myName);
+    //     myHeading.textContent = `Mozilla is cool, ${myName}`;
+    // }
+
+    // if (!localStorage.getItem("name")) {
+    //     setUserName();
+    // } else {
+    //     const storedName = localStorage.getItem("name");
+    //     myHeading.textContent = `Mozilla is cool, ${storedName}`;
+    // }
 });
---
+++ main.js
@@ -0,0 +1,4 @@
+// // Store a reference to the <h2> in a variable
+// const myHeading = document.querySelector("h1");
+// // Update the text content of the <h2>
+// myHeading.textContent = "Hello world!";
