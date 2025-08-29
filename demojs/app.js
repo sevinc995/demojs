959339546df352f02b1d776f4707e2a74e012a52
sevinc995 <sevincseferova25@gmail.com>
2025-08-29 11:03:35 +0000

    change

--- demojs/app.js
+++ demojs/app.js
@@ -1,32 +1,41 @@
 "use strict";
 $(document).ready(function () {
-    // const myImage = document.querySelector("img");
-    // // sekli klik etdikde sekli deyisir
-    // myImage.addEventListener("click", () => {
-    //     const mySrc = myImage.getAttribute("src");
-    //     if (mySrc === "images/firefox-icon.jpeg") {
-    //         myImage.setAttribute("src", "images/firefox2.jpeg");
-    //     } else {
-    //         myImage.setAttribute("src", "images/firefox-icon.jpeg");
-    //     }
-    // });
+    const myImage = document.querySelector("img");
+    // sekli klik etdikde sekli deyisir
+    myImage.addEventListener("click", () => {
+        const mySrc = myImage.getAttribute("src");
+        if (mySrc === "images/firefox-icon.jpeg") {
+            myImage.setAttribute("src", "images/firefox2.jpeg");
+        } else {
+            myImage.setAttribute("src", "images/firefox-icon.jpeg");
+        }
+    });
 
 
-    // // yaziya klik etdikde ustunden xett cekir ve qirmizi renge boyayir.
-    // const listItems = document.querySelectorAll("li");
+    // yaziya klik etdikde ustunden xett cekir ve qirmizi renge boyayir.
+    const listItems = document.querySelectorAll("li");
 
-    // function toggleDone(e) {
-    //     if (!e.target.className) {
-    //         e.target.className = "done";
-    //     } else {
-    //         e.target.className = "";
-    //     }
+    function toggleDone(e) {
+        if (!e.target.className) {
+            e.target.className = "done";
+        } else {
+            e.target.className = "";
+        }
+    }
+
+    listItems.forEach((item) => {
+        item.addEventListener("click", toggleDone);
+    });
+
+    // function setUserName() {
+    //     const myName = prompt("Please enter your name.");
+    //     localStorage.setItem("name", myName);
+    //     myHeading.textContent = `Mozilla is cool, ${myName}`;
     // }
-
-    // listItems.forEach((item) => {
-    //     item.addEventListener("click", toggleDone);
-    // });
-
-
-   
+    // if (!localStorage.getItem("name")) {
+    //     setUserName();
+    // } else {
+    //     const storedName = localStorage.getItem("name");
+    //     myHeading.textContent = `Mozilla is cool, ${storedName}`;
+    // }
 });
---
+++ main.js
@@ -0,0 +1,2 @@
+// let myButton = document.querySelector("button");
+// let myHeading = document.querySelector("h1");
