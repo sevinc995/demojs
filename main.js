4af3f5328aeac15686323b0d8b8ec77b9a7e38bf
sevinc995 <sevincseferova25@gmail.com>
2025-08-29 10:51:58 +0000

    commit

--- main.js
+++
@@ -1,4 +0,0 @@
-// // Store a reference to the <h2> in a variable
-// const myHeading = document.querySelector("h1");
-// // Update the text content of the <h2>
-// myHeading.textContent = "Hello world!";
